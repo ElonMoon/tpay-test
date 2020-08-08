6b5da0acdc2380cd7df684d0a68fbd1f822339f7
joohyun <elonmoon@gmail.com>
2020-08-08 14:46:15 +0900

    [SETTINGS]app, shop.admin 등록

--- app/shop/admin.py
+++ app/shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tag, Product, ProductOption
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductOption)
+class ProductOptionAdmin(admin.ModelAdmin):
+    pass
