4018bb147c0c9e40d4c4a24c0ab6564add6b1eec
joohyun <elonmoon@gmail.com>
2020-08-09 11:04:41 +0900

    [SHOP-TEST]Product 생성 테스트 추가

--- app/shop/tests.py
+++ app/shop/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import json
+import pytest
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.reverse import reverse
+
+
+@pytest.mark.django_db
+def test_shop_product_post(client):
+    url = reverse(viewname='product-list')
+    data = {
+        'name': 'TestProduct',
+        'option_set': [
+            {
+                'name': 'TestOption1',
+                'price': 1000
+            },
+            {
+                'name': 'TestOption2',
+                'price': 500
+            },
+            {
+                'name': 'TestOption3',
+                'price': 0
+            }
+        ],
+        'tag_set': [
+            {
+                'pk': 1,
+                'name': 'ExistTag'
+            },
+            {
+                'name': 'NewTag'
+            }
+        ]
+    }
+    response = client.post(url, json.dumps(data), content_type='application/json')
+    assert response.status_code == status.HTTP_201_CREATED
